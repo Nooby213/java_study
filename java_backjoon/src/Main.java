d6b3bdcf52c67953bd65380d44a05183d0f68c0b
이승민 <lsm5631@naver.com>
2024-04-04 17:54:43 +0900

    240404 백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -1,62 +1,69 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static List<String> words = new LinkedList<>();
-    static int N;
-    static int K;   // 가르칠 수 있는 알파벳 개수 K, 0 <= K <= 26
-    static int maxCnt = 0;  // 배울 수 있는 단어의 최댓값
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        // 주어지는 단어의 개수 N, 0 < N <= 50
-        N = Integer.parseInt(st.nextToken());
-        // 가르칠 수 있는 알파벳 개수 K, 0 <= K <= 26
-        K = Integer.parseInt(st.nextToken());
-        Set<Character> antatica = new HashSet<>();
-        antatica.add('a');
-        antatica.add('n');
-        antatica.add('t');
-        antatica.add('i');
-        antatica.add('c');
-        // K가 5 미만이면 antatica도 못배움
-        if (K < 5) {
-            System.out.println(0);
-        } else if (K == 26) {
-            System.out.println(N);
-        } else {
+    static int n, k;
+    static int max = Integer.MIN_VALUE;
+    static boolean[] visited;
+    static String[] word;
 
-            // 배울 단어들 리스트만들기
-            for (int i = 0; i < N; i++) {
-                words.add(br.readLine());
-            }
-//            words.sort(Comparator.comparingInt(String::length));
-            dfs(0,0,5,new HashSet<>(antatica));
-            System.out.println(maxCnt);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+        k = scan.nextInt();
+
+        scan.nextLine();
+        word = new String[n];
+        for(int i = 0; i < n; i++) {
+            String str = scan.nextLine();
+            str = str.replace("anta", "");
+            str = str.replace("tica", "");
+            word[i] = str;
         }
+
+        if(k < 5) { //a c i n t의 개수가 5개이므로
+            System.out.println("0");
+            return;
+        } else if(k == 26) { //모든 알파벳을 다 읽을 수 있다.
+            System.out.println(n);
+            return;
+        }
+
+        visited = new boolean[26]; //각 알파벳을 배웠는지 체크
+        visited['a' - 'a'] = true;
+        visited['c' - 'a'] = true;
+        visited['i' - 'a'] = true;
+        visited['n' - 'a'] = true;
+        visited['t' - 'a'] = true;
+
+        backtracking(0, 0);
+        System.out.println(max);
     }
 
-    static void dfs(int n, int cnt, int len, Set<Character> aset) {
-        // 단어 개수가 K보다 크다면 멈춤
-        if (len > K) {
+    public static void backtracking(int alpha, int len) {
+        if(len == k - 5) {
+            int count = 0;
+            for(int i = 0; i < n; i++) { //뽑은 알파벳으로 몇개의 단어를 읽을 수 있는지 카운트.
+                boolean read = true;
+                for(int j = 0; j < word[i].length(); j++) {
+                    if(!visited[word[i].charAt(j) - 'a']) {
+                        read = false;
+                        break;
+                    }
+                }
+                if(read) count++;
+            }
+            max = Math.max(max, count);
             return;
         }
-        // 끝 단어까지 다 배웠다면 최대값 갱신
-        if (n == N) {
-            maxCnt = Math.max(maxCnt, cnt);
-            return;
+
+        for(int i = alpha; i < 26; i++) {
+            if(visited[i] == false) {
+                visited[i] = true;
+                backtracking(i, len + 1);
+                visited[i] = false;
+            }
         }
-        if (cnt + N - n <= maxCnt){
-            return;
-        }
-        // 안 배운다.
-        dfs(n + 1, cnt, aset.size(), new HashSet<>(aset));
-        // 배운다면 단어장에 기록
-        String tempWord = words.get(n);
-        for (int i = 0; i < tempWord.length(); i++) {
-            aset.add(tempWord.charAt(i));
-        }
-        dfs(n + 1, cnt + 1, aset.size(), new HashSet<>(aset));
     }
 }
--- java_backjoon/src/gold/Gold_1062_Visited.java
+++ java_backjoon/src/gold/Gold_1062_Visited.java
@@ -1,4 +1,22 @@
 package gold;
+import java.util.*;
+import java.io.*;
 
 public class Gold_1062_Visited {
+    static int n;
+    static int k;
+    static int max = Integer.MIN_VALUE;
+    static String[] words;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+
+        }
+
+    }
 }
