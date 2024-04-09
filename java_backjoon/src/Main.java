699a1df76c0667be2dad4fc2bd26e8979ae158fd
이승민 <lsm5631@naver.com>
2024-04-09 16:18:48 +0900

    240409 백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -2,38 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n;
-    static int k;
-    static Set<String> permutations;
-    static String[] cards;
-    static boolean[] visited;
-    static boolean[] startPoint = new boolean[100];
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        k = Integer.parseInt(br.readLine());
-        cards = new String[n];
-        for (int i = 0; i < n; i++) {
-            cards[i] = br.readLine();
-        }
-        visited = new boolean[n];
-        permutations = new HashSet<>();
-        makePermutation("", 0);
-        System.out.println(permutations.size());
-    }
 
-    static void makePermutation(String temp, int cnt) {
-        if (cnt == k) {
-            permutations.add(temp);
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                makePermutation(temp + cards[i], cnt + 1);
-                visited[i] = false;
-            }
-        }
     }
 }
