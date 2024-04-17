d6dbd70355be2491e8b0fadd48f3556cde4bc343
Nooby213 <lsm5631@naver.com>
2024-04-17 23:56:55 +0900

    240417 백준문제풀이

--- java_backjoon/src/gold/Gold_11729.java
+++ java_backjoon/src/gold/Gold_11729.java
@@ -1,13 +1,13 @@
-import java.util.*;
+package gold;
 import java.io.*;
 
-public class Main {
+public class Gold_11729 {
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-
+        // 횟수가 2의 n제곱 씩 증가함
         sb.append((int) Math.pow(2, n) - 1).append('\n');
         hanoi(n, 1, 2, 3);
         System.out.println(sb);
