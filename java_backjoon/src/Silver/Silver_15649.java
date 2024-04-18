eba6d36d1d7484fed743d964a84044912af83a17
이승민 <lsm5631@naver.com>
2024-04-18 17:34:54 +0900

    240418 백준문제풀이

--- java_backjoon/src/Silver/Silver_15649.java
+++ java_backjoon/src/Silver/Silver_15649.java
@@ -1,7 +1,8 @@
-import java.util.*;
-import java.io.*;
+package Silver;
 
-public class Main {
+import java.util.*;
+
+public class Silver_15649 {
     static boolean[] visited;
     static int n;
     static int m;
@@ -21,9 +22,12 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             if (visited[i] == false) {
                 visited[i] = true;
-//                StringBuilder newSb = new StringBuilder(sb);
-//                comb(cnt + 1, newSb.append(i).append(" "));
-                comb(cnt + 1, new StringBuilder(sb).append(i).append(" "));
+                StringBuilder newSb = new StringBuilder(sb);
+                comb(cnt + 1, newSb.append(i).append(" "));
+                // 안되는 코드
+//                comb(cnt + 1, new StringBuilder(sb.append(i).append(" ")));
+                // 되는 코드 (복사하는 값에 붙여넣으면 원본인 sb가 바껴서 들어감)
+//                comb(cnt + 1, new StringBuilder(sb).append(i).append(" "));
                 visited[i] = false;
             }
         }
