0cb26fe7a0b375fb5c19020a3a614cdab7886d77
이승민 <lsm5631@naver.com>
2024-04-29 18:00:44 +0900

    240429 백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -2,47 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static Long sum = 0L;
-    static int n;
-    static int mod = 1000000000;
-    static List<Long> temp;
-
-    static List<Long> start = new ArrayList<>();
-    static {
-        for (int i = 0; i < 10; i++) {
-            start.add(1L);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[] numbers = new int[n+1];
+        int[] dp = new int[n+1];
+        for (int i = 1; i < n + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
-    }
-    public static void main(String[] args) throws Exception {
-        long start = System.currentTimeMillis();
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            stairNum(i);
+        dp[1] = numbers[1];
+        if (n > 1) {
+            dp[2] = numbers[1] + numbers[2];
         }
-        for (int i = 1; i < 10; i++) {
-            sum += temp.get(i);
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + numbers[i], dp[i - 3] + numbers[i - 1] + numbers[i]));
         }
-        System.out.println(sum % mod);
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
-    }
-
-    static void stairNum(int i) {
-        if (i == 0) {
-            temp = new ArrayList<>(start);
-            return;
-        }
-        List<Long> arr = new ArrayList<>();
-        for (int j = 0; j < 10; j++) {
-            if (j == 0) {
-                arr.add(temp.get(1) % mod);
-            } else if (j == 9) {
-                arr.add(temp.get(8) % mod);
-            } else {
-                arr.add((temp.get(j + 1) + temp.get(j - 1)) % mod);
-            }
-        }
-        temp = new ArrayList<>(arr);
+        System.out.println(dp[n]);
+        System.out.println(Arrays.toString(dp));
     }
 }
