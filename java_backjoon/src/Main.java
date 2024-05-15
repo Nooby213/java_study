9689d000555f0f1676e1d0c12fbacb4231689c09
Nooby213 <lsm5631@naver.com>
2024-05-16 00:58:06 +0900

    240516 백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -6,25 +6,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[] numbers = new int[n];
-        int[] sum = new int[n];
-        int cnt = 1;
-        st = new StringTokenizer(br.readLine());
-        numbers[0] = Integer.parseInt(st.nextToken());
-        sum[0] = numbers[0];
-        for (int i = 1; i < n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-            sum[i] = numbers[i] + sum[i - 1];
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int tempSum = sum[j] - sum[j - i];
-                if (tempSum % m == 0) {
-                    cnt++;
-                }
-            }
-        }
-        System.out.println(cnt);
+        String expression = br.readLine();
+        char[] arr = expression.toCharArray();
+
+        System.out.println(Arrays.toString(arr));
     }
 }
