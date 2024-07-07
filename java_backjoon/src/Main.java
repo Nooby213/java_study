8a5ef0713580050191309c4e1bf8a7aa37a2115c
Nooby213 <lsm5631@naver.com>
2024-07-08 02:13:59 +0900

    백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -1,4 +1,3 @@
-import javax.imageio.metadata.IIOMetadataFormatImpl;
 import java.util.*;
 import java.io.*;
 
@@ -23,47 +22,21 @@ public class Main {
         }
         // 날짜
         int d = Integer.parseInt(br.readLine());
-        // 캐릭 수 * 힘 합
-        int sum = 0;
+
+        // dp 배열 초기화
+        int[][][] dp = new int[d + 1][n][n];
         for (int i = 0; i < n; i++) {
-            sum += character[i] * str[i];
-        }
-        // 매일 기록
-        int[][] dp = new int[d + 1][n];
-        List<int[]> days = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            days.add(Arrays.copyOf(character, n));
-        }
-        Arrays.fill(dp[0], sum);
-        // 최대값 초기화
-        int max = sum;
-        for (int i = 1; i <= d; i++) {
-            dp[i][0] = sum;
-            System.out.println("----------" + i);
-            System.out.println(Arrays.toString(days.get(0)));
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j - 1] - str[j - 1] + str[j];
-                dp[i][j] = train(days.get(j), j, dp[i - 1][j - 1]);
-                max = Math.max(max, dp[i][j]);
-//                for (int[] day : days) {
-//                    System.out.println(Arrays.toString(day));
-//                }
+            for (int j = 0; j < n; j++) {
+                dp[0][i][j] = character[j]; // dp[0][i][j]를 character 배열로 초기화
             }
         }
-        System.out.println(max);
 
-//        for (int[] ints : dp) {
-//            System.out.println(Arrays.toString(ints));
-//        }
-    }
-
-    static int train(int[] character, int level, int sum) {
-        System.out.println(Arrays.toString(character));
-        if (character[level - 1] == 0) {
-            return sum;
+        // 초기화된 dp 배열 출력 (디버깅 용)
+        for (int day = 0; day <= d; day++) {
+            System.out.println("Day " + day + ":");
+            for (int i = 0; i < n; i++) {
+                System.out.println(Arrays.toString(dp[day][i]));
+            }
         }
-        character[level - 1]--;
-        character[level]++;
-        return sum + str[level] - str[level - 1];
     }
 }
