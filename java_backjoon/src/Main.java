c0e575f50fa53738f6a402601c9759a2b2fbdc6b
이승민 <lsm5631@naver.com>
2024-07-11 15:33:13 +0900

    백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -2,41 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] str;
+    static int n;
+    static int[][] arr;
+    static List<List<List<Integer>>> fish;
+    static int shark = 2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        // 레벨 n, n당 캐릭터 수
-        int n = Integer.parseInt(br.readLine());
-        int[] character = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            character[i] = Integer.parseInt(st.nextToken());
-        }
-        // 힘
-        str = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            str[i] = Integer.parseInt(st.nextToken());
-        }
-        // 날짜
-        int d = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
 
-        // dp 배열 초기화
-        int[][][] dp = new int[d + 1][n][n];
+        fish = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            fish.add(new ArrayList<>());
+        }
+
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                dp[0][i][j] = character[j]; // dp[0][i][j]를 character 배열로 초기화
+                int temp = Integer.parseInt(st.nextToken());
+                arr[i][j] = temp;
+                if (temp != 0 && temp != 9) {
+                    fish.get(temp).add(new ArrayList<>(Arrays.asList(i, j)));
+                }
             }
         }
 
-        // 초기화된 dp 배열 출력 (디버깅 용)
-        for (int day = 0; day <= d; day++) {
-            System.out.println("Day " + day + ":");
-            for (int i = 0; i < n; i++) {
-                System.out.println(Arrays.toString(dp[day][i]));
+        System.out.println(fish);
+
+        while (true) {
+            int dis = Integer.MAX_VALUE;
+            for (int i = 0; i < ; i++) {
+                
             }
         }
+
     }
 }
