d86cdadf9c1712b0f1a73d4fe80aa581438247ab
이승민 <lsm5631@naver.com>
2024-07-17 17:55:23 +0900

    백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         name = new int[n];
-        dp = new long[n][m];
+        dp = new long[n][m + 1];
         for (int i = 0; i < n; i++) {
             name[i] = Integer.parseInt(br.readLine());
         }
@@ -35,14 +35,21 @@ public class Main {
             return dp[i][j];
         }
 
-        // 지금 줄에 추가
-        if (j + name[i] < m) {
-            dp[i][j] = Math.min(dp[i][j], deathNote(i + 1, j + name[i]));
+        // 현재 줄에 이름을 추가할 수 있는 경우
+        if (j + name[i] <= m) {
+            if (j + name[i] == m) { // 줄을 정확히 채우는 경우
+                dp[i][j] = Math.min(dp[i][j], deathNote(i + 1, 0));
+            } else { // 현재 줄에 이름을 추가하는 경우
+                dp[i][j] = Math.min(dp[i][j], deathNote(i + 1, j + name[i] + 1));
+            }
         }
 
-        // 다음 줄에 추가
-        dp[i][j] = (long) Math.min(dp[i][j], Math.pow(m - j + 1, 2) + deathNote(i, 0));
-
+        // 다음 줄에 이름을 추가하는 경우
+        if (j != 0) { // 현재 줄이 비어있지 않다면 남은 칸의 비용을 계산
+            dp[i][j] = Math.min(dp[i][j], (long) Math.pow(m - j + 1, 2) + deathNote(i, 0));
+        } else { // 현재 줄이 비어있다면 이름을 추가
+            dp[i][j] = Math.min(dp[i][j], deathNote(i + 1, name[i] + 1));
+        }
 
         return dp[i][j];
     }
