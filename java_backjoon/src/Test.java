0c99091e5851d475b45beb3075d0cac28080b206
이승민 <lsm5631@naver.com>
2024-08-08 17:39:51 +0900

    백준 문제 풀이 q rlease enter the commit message for your changes. Lines starting

--- java_backjoon/src/Test.java
+++ java_backjoon/src/Test.java
@@ -1,38 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Comparator;
+import java.util.TreeSet;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        Map<String, Integer> map = new HashMap<>();
-        char[] a = new char[]{1, 2, 3, 4};
-        char[] b = new char[]{1, 2, 3, 4};
-        String aa = String.valueOf(a);
-        String bb = String.valueOf(b);
-        map.put(aa, 1);
-        System.out.println(map.get(bb));
-        System.out.println(aa.equals(bb));
-        System.out.println(aa == bb);
-//        System.out.println(a.equals(b));
-//        System.out.println(Arrays.equals(a, b));
-//        System.out.println(a == b);
+    public static void main (String[] args) {
+        TreeSet<Integer> treeSet1 = new TreeSet<>((o1, o2) -> o1 - o2);
+        TreeSet<Integer> treeSet2 = new TreeSet<>((o1, o2) -> o2 - o1);
+        TreeSet<Node> treeSet3 = new TreeSet<>(Comparator.comparing(n -> n.b));
+        for (int i = 0; i < 10; i++) {
+            treeSet1.add(i);
+            treeSet2.add(i);
+        }
+        System.out.println(treeSet1);
+        System.out.println(treeSet2);
 
+        treeSet3.add(new Node("200", 999));
+        for (int i = 0; i < 10; i++) {
+            treeSet3.add(new Node(String.valueOf(i), i+100));
+        }
 
-/*
-    Java의 HashMap은 키로 사용되는 객체의 hashCode()와 equals() 메서드를 사용하여 키를 비교하고 해시 테이블에 저장된 항목을 검색합니다.
-    이를 통해 HashMap은 동일한 내용을 가진 문자열이 다른 객체로 생성되더라도 올바르게 작동할 수 있습니다.
+        System.out.println(treeSet3);
+    }
+    static class Node {
+        String a;
+        int b;
 
-    hashCode() 메서드:
-    hashCode() 메서드는 객체의 해시 코드를 반환합니다. HashMap은 이 해시 코드를 사용하여 객체를 버킷(bucket)에 할당합니다.
-    String 클래스는 hashCode() 메서드를 오버라이드하여 문자열의 내용을 기반으로 해시 코드를 계산합니다.
-    따라서 동일한 내용을 가진 문자열은 동일한 해시 코드를 갖습니다.
+        public Node(String a, int b) {
+            this.a = a;
+            this.b = b;
+        }
 
-    equals() 메서드:
-    HashMap은 같은 해시 코드를 가진 객체가 여러 개 있을 수 있으므로, equals() 메서드를 사용하여 실제로 동일한지를 확인합니다.
-    String 클래스는 equals() 메서드를 오버라이드하여 문자열의 내용을 비교합니다.
-    따라서 동일한 내용을 가진 문자열은 equals() 메서드를 사용하면 true를 반환합니다.
-    이를 통해, HashMap은 동일한 내용을 가진 다른 String 객체를 키로 사용하더라도 이를 동일한 키로 인식할 수 있습니다.
-*/
+        @Override
+        public String toString() {
+            return a + ", " + b;
+        }
     }
 }
