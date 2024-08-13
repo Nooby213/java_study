58f52435be250189e13707c0713dc0b70d45a4a8
Nooby213 <lsm5631@naver.com>
2024-08-14 00:44:23 +0900

    문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -1,5 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
+import java.io.*;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<List<Integer>> node = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
+            node.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            node.get(a).add(b);
+            node.get(b).add(a);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+
+        }
     }
 }
