a6b7adbfbdcf2ec54bbd633449e2c86c7dda9289
이승민 <lsm5631@naver.com>
2024-08-27 17:52:12 +0900

    백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -1,51 +1,21 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int count = -1;
-    static int n;
-    static int[] visited = new int[10];
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                visited[j] = 1;
-                make(i, j, 1);
-                visited[j] = 0;
-                if (count >= n) {
-                    return;
-                }
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[] weight = new boolean[n * 500 + 1];
+        List<Integer> choo = new ArrayList<>();
+        int[] visited = new int[501];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int w = Integer.parseInt(st.nextToken());
+            visited[w]++;
+            choo.add(w * visited[w]);
         }
-        System.out.println(-1);
-    }
-
-    static void make(int len, int num, int cnt) {
-        if (count >= n) {
-            return;
-        }
-        if (len == cnt) {
-            count++;
-            if (count == n) {
-                for (int k = 9; k >= 0; k--) {
-                    if (visited[k] == 1) {
-                        sb.append(k);
-                    }
-                }
-                System.out.println(sb);
-            }
-            return;
-        }
-
-        for (int i = 0; i < num; i++) {
-            if (visited[i] == 0) {
-                visited[i] = 1;
-                make(len, i, cnt + 1);
-                visited[i] = 0;
-            }
-        }
+        System.out.println(choo);
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
     }
 }
---
+++ java_backjoon/src/gold/Gold_1038.java
@@ -0,0 +1,52 @@
+package gold;
+import java.util.*;
+
+public class Gold_1038 {
+    static int count = -1;
+    static int n;
+    static int[] visited = new int[10];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                visited[j] = 1;
+                make(i, j, 1);
+                visited[j] = 0;
+                if (count >= n) {
+                    return;
+                }
+            }
+        }
+        System.out.println(-1);
+    }
+
+    static void make(int len, int num, int cnt) {
+        if (count >= n) {
+            return;
+        }
+        if (len == cnt) {
+            count++;
+            if (count == n) {
+                for (int k = 9; k >= 0; k--) {
+                    if (visited[k] == 1) {
+                        sb.append(k);
+                    }
+                }
+                System.out.println(sb);
+            }
+            return;
+        }
+
+        for (int i = 0; i < num; i++) {
+            if (visited[i] == 0) {
+                visited[i] = 1;
+                make(len, i, cnt + 1);
+                visited[i] = 0;
+            }
+        }
+    }
+}
+
