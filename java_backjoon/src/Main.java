6184580f55aa62205947250b2dbc071290b53391
Nooby213 <lsm5631@naver.com>
2024-09-09 02:16:17 +0900

    백준문제풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -1,45 +1,82 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static boolean[][] board = new boolean[5][5];
+    static boolean[][] visited = new boolean[5][5];
+    static int ans = 0;
+    static int[] di = new int[]{-1, 1, 0, 0};
+    static int[] dj = new int[]{0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int max = n * 500;
-        boolean[][] dp = new boolean[n + 1][max + 1];
-        int[] weight = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            weight[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < 5; j++) {
+                if (line.charAt(j) == 'S') board[i][j] = true;
+            }
         }
-        dp[0][0] = true;
 
-        for (int i = 1; i <= n; i++) {
-            int w = weight[i - 1];
-            for (int j = 0; j <= max; j++) {
-                if (dp[i - 1][j]) {
-                    dp[i][j] = true;
-                    dp[i][j + w] = true;
-                    dp[i][Math.abs(j - w)] = true;
+        comb(0, 0, 0, 0);
+        System.out.println(ans);
+    }
+
+    static void comb(int cnt, int sCnt, int startY, int startX) {
+        if (cnt == 7) {
+            if (sCnt >= 4 && check()) {
+                ans++;
+            }
+            return;
+        }
+
+        for (int i = startY; i < 5; i++) {
+            for (int j = ((startY == i) ? startX : 0); j < 5; j++) {
+                if (!visited[i][j]) {
+                    visited[i][j] = true;
+                    comb(cnt + 1, sCnt + (board[i][j] ? 1 : 0), i, j + 1);
+                    visited[i][j] = false;
+                }
+            }
+        }
+    }
+
+    static boolean check() {
+        int cnt = 0;
+        boolean[][] checkVisited = new boolean[5][5];
+        Queue<int[]> q = startQ(checkVisited);
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int i = now[0];
+            int j = now[1];
+            cnt++;
+            for (int k = 0; k < 4; k++) {
+                int ni = i + di[k];
+                int nj = j + dj[k];
+                if (ni >= 0 && ni < 5 && nj >= 0 && nj < 5) {
+                    if (visited[ni][nj] && !checkVisited[ni][nj]) {
+                        q.add(new int[]{ni, nj});
+                        checkVisited[ni][nj] = true;
+                    }
+                }
+            }
+        }
+        return cnt == 7;
+    }
+
+    static Queue<int[]> startQ(boolean[][] checkVisited) {
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (visited[i][j] == true) {
+                    q.add(new int[]{i, j});
+                    checkVisited[i][j] = true;
+                    return q;
                 }
             }
         }
 
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < m; i++) {
-            int choo = Integer.parseInt(st.nextToken());
-            if (choo > max) {
-                sb.append("N ");
-            } else if (dp[n][choo]) {
-                sb.append("Y ");
-            } else {
-                sb.append("N ");
-            }
-        }
-        System.out.println(sb);
+        return q;
     }
 }
--- java_backjoon/src/Test.java
+++ java_backjoon/src/Test.java
@@ -1,10 +1,78 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
+import java.io.*;
 
 public class Test {
-    public static void main (String[] args) {
-        System.out.println(10e9);
+    static char[][] map = new char[5][5];
+    static boolean[][] visited = new boolean[5][5];
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < 5; j++) {
+                map[i][j] = row.charAt(j);
+            }
+        }
+        comb(0, 0, 0, 0);
+        System.out.println(answer);
+    }
+
+    public static void comb(int cnt, int yCnt, int startY, int startX) {
+        if (cnt == 7) {
+            if (yCnt < 4 && check()) {
+                answer++;
+            }
+            return;
+        }
+
+        for (int i = startY; i < 5; i++) {
+            for (int j = (i == startY ? startX : 0); j < 5; j++) {
+                visited[i][j] = true;
+                comb(cnt + 1, yCnt + (map[i][j] == 'Y' ? 1 : 0), i, j + 1);
+                visited[i][j] = false;
+            }
+        }
+    }
+
+    public static boolean check() {
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] checkVisited = new boolean[5][5];
+
+        outerLoop:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (visited[i][j]) {
+                    q.add(new int[]{i, j});
+                    checkVisited[i][j] = true;
+                    break outerLoop;
+                }
+            }
+        }
+
+        int cnt = 0;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            cnt++;
+            int x = now[0];
+            int y = now[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5) {
+                    if (visited[nx][ny] && !checkVisited[nx][ny]) {
+                        q.add(new int[]{nx, ny});
+                        checkVisited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+
+        return cnt == 7;
     }
 }
