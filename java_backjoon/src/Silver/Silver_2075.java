adeb1f231183909420a4ae7caee60eea8cd4163b
Nooby213 <lsm5631@naver.com>
2024-09-21 11:42:03 +0900

    백준문제풀이

--- java_backjoon/src/Silver/Silver_2075.java
+++ java_backjoon/src/Silver/Silver_2075.java
@@ -1,7 +1,9 @@
+package Silver;
+
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Silver_2075 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
