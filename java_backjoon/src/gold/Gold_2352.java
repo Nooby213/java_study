abaa859545f2e461dd89a177e830427768f23d4b
Nooby213 <lsm5631@naver.com>
2024-10-18 00:07:55 +0900

    알고리즘

--- java_backjoon/src/gold/Gold_2352.java
+++ java_backjoon/src/gold/Gold_2352.java
@@ -1,7 +1,9 @@
+package gold;
+
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Gold_2352 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
