e916401a3dc6ece2b1b4d86bb9f423c2771d57ee
Nooby213 <lsm5631@naver.com>
2024-10-23 01:59:59 +0900

    알고리즘풀이

--- java_backjoon/src/Main.java
+++ java_backjoon/src/Main.java
@@ -2,47 +2,105 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static long mod = 1000000;
+    static class Edge implements Comparable<Edge> {
+        int nextV;
+        int weight;
 
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
-        System.out.println(fibo(n));
+        public Edge(int nextV, int weight) {
+            this.nextV = nextV;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge other) {
+            return Integer.compare(this.weight, other.weight);
+        }
+
+        @Override
+        public String toString() {
+            return nextV + " : " + weight;
+        }
     }
-    static long[][] power(long[][] a, long n) {
-        if (n == 1) {
-            return a;
+
+    static class Node {
+        List<Edge> edges = new ArrayList<>();
+
+        public void addEdge(int v, int w) {
+            edges.add(new Edge(v, w));
         }
-        if (n == 0) {
-            return new long[][]{{1, 0}, {0, 1}};
+
+        public void sortEdges() {
+            Collections.sort(edges);
         }
-        if (n % 2 == 0) {
-            long[][] halfPower = power(a, n / 2);
-            return multiple(halfPower, halfPower);
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (Edge edge : edges) {
+                sb.append(edge.toString()).append("\n");
+            }
+            return sb.toString();
+        }
+    }
+
+    static int v;
+    static int e;
+    static int p;
+    static List<Node> nodes;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken()); // 정점 개수
+        e = Integer.parseInt(st.nextToken()); // 간선 개수
+        p = Integer.parseInt(st.nextToken()); // 건우 정점
+        nodes = new ArrayList<>();
+        for (int i = 0; i <= v; i++) {
+            nodes.add(new Node());
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()); // 정점1
+            int b = Integer.parseInt(st.nextToken()); // 정점2
+            int c = Integer.parseInt(st.nextToken()); // 거리
+            nodes.get(a).addEdge(b, c);
+            nodes.get(b).addEdge(a, c);
+        }
+
+//        항상 1번 정점에서 P번과 V번 정점으로 갈 수 있는 경로가 존재한다.
+        int toV = saveMinJun(1, v);
+        int toP = saveMinJun(1, p);
+        int pToV = saveMinJun(p, v);
+        if (toV >= toP + pToV) {
+            System.out.println("SAVE HIM");
         } else {
-            return multiple(a, power(a, n - 1));
+            System.out.println("GOOD BYE");
         }
     }
 
-    static long[][] multiple(long[][] a, long[][] b) {
-        long[][] res = new long[2][2];
+    static int saveMinJun(int start, int end) {
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        int[] distance = new int[v + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[start] = 0;
+        pq.add(new Edge(start, 0));
 
-        res[0][0] = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % mod;
-        res[0][1] = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % mod;
-        res[1][0] = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % mod;
-        res[1][1] = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % mod;
-        return res;
-    }
+        while (!pq.isEmpty()) {
+            Edge cur = pq.poll();
+            int curNode = cur.nextV;
+            int curWeight = cur.weight;
+            if (curWeight > distance[curNode]) continue;
 
-    static long fibo(long n) {
-        long[][] a = new long[][]{{1, 1}, {1, 0}};
-
-        if (n == 0) {
-            return 0;
+            for (Edge edge : nodes.get(curNode).edges) {
+                int nextNode = edge.nextV;
+                int nextWeight = curWeight + edge.weight;
+                if (nextWeight < distance[nextNode]) {
+                    distance[nextNode] = nextWeight;
+                    pq.add(new Edge(nextNode, nextWeight));
+                }
+            }
         }
-
-        long[][] res = power(a, n - 1);
-
-        return res[0][0];
+        return distance[end];
     }
 }
