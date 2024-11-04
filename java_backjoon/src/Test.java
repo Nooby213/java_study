4230fdf351c599dc4f21d4ec78af408da9353191
Nooby213 <lsm5631@naver.com>
2024-11-04 23:45:19 +0900

    알고리즘

--- java_backjoon/src/Test.java
+++ java_backjoon/src/Test.java
@@ -2,33 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Test {
-    static int n;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-
-        int preDiff = Integer.parseInt(st2.nextToken()) - Integer.parseInt(st1.nextToken());
-        int cnt = Math.abs(preDiff);
-
-        for (int i = 1; i < n; i++) {
-            int tap = Integer.parseInt(st1.nextToken());
-            int toBe = Integer.parseInt(st2.nextToken());
-
-            int curDiff = toBe - tap;
-
-            if (preDiff * curDiff < 0) {
-                cnt += Math.abs(preDiff);
-            } else if (Math.abs(preDiff) >= Math.abs(curDiff)) {
-                cnt += Math.abs(preDiff) - Math.abs(curDiff);
-            }
-
-            preDiff = curDiff;
-        }
-
-        cnt += Math.abs(preDiff);
-        System.out.println(cnt);
+    public static void main(String[] args) {
+        System.out.println((char)('1' + '1'));
     }
 }
--- java_backjoon/src/gold/Gold_2133.java
+++ java_backjoon/src/gold/Gold_2133.java
@@ -1,6 +1,8 @@
+package gold;
+
 import java.util.Scanner;
 
-public class Main {
+public class Gold_2133 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
